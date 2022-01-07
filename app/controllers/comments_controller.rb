6dfaa45a567ffa36c4b168a1ad6c636f7ef9bc3e
Emiliano Lopez <emilianolch@gmail.com>
2022-01-06 23:13:21 -0300

    new comment mail notification

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,13 @@
 class CommentsController < ApplicationController
   before_action :set_post
   
-  def new
-    @comment = @post.comments.new
-  end
-
   def create
     @comment = @post.comments.new(comment_params)
 
     respond_to do |format|
       if @comment.save
+        CommentsMailer.submitted(@comment).deliver_later
+
         format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
         format.turbo_stream {
           render turbo_stream: turbo_stream.append('comments', @comment)
