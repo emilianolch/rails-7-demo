549bfe38e53c39c2a353ef260f6d7a476ab5403b
Emiliano Lopez <emilianolch@gmail.com>
2022-01-06 23:23:23 -0300

    new comment action cable response

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,11 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        CommentsMailer.submitted(@comment).deliver_later
+        #CommentsMailer.submitted(@comment).deliver_later
 
         format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
         format.turbo_stream {
-          render turbo_stream: turbo_stream.append('comments', @comment)
+          #render turbo_stream: turbo_stream.append('comments', @comment)
         }
       else
         format.html { render :new, status: :unprocessable_entity }
